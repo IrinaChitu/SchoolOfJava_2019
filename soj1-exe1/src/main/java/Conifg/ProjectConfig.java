a69288ed98246f32b289768a385b6240d03b6f52
Irina <yours@gmail.com>
2019-04-08 19:56:35 +0300

    Started working with Spring

---
+++ soj1-exe1/src/main/java/Conifg/ProjectConfig.java
@@ -0,0 +1,19 @@
+package Conifg;
+
+
+import Objects.Pisica;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // adnotare (= marcam o caracteristica a unui alt element): clasa de configurare
+public class ProjectConfig {
+    //declaram un bean = cream o instanta si o punem in interiorul contextului, care ajunge in spring
+
+    @Bean
+    public Pisica pisica() { // reprezinta instanta | nu are un nume standard pt o metoda (de obicei cu verb)
+        Pisica c = new Pisica();
+        c.name = "Tom";
+        return c;
+    }
+
+}
---
+++ soj1-exe1/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import Conifg.ProjectConfig;
+import Objects.Pisica;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class)) { // try cu resurse
+            Pisica p = context.getBean(Pisica.class); // preia referinta la un obiect creat (e un singur obiect creat => daca mai creez o pisica, imi da ac referinta
+            System.out.println(p.name);
+        }
+    }
+}
