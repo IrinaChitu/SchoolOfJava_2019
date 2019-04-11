624f4bd82ee992dc77dfea8239e6097e32be2d62
Irina <chitu.irina.nicoleta@gmail.com>
2019-04-11 18:51:50 +0300

    Started working with Spring framework

---
+++ soj2-StartingWithSpring/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+import beans.TransactionRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+
+    public TransactionRepository transactionRepository() { // clasa pe care vreau sa o fac bean | sa o creez si sa o accesez
+         TransactionRepository transactionRepository = new TransactionRepository();
+         transactionRepository.setNume("Transaction 1");
+         return transactionRepository;
+    }
+
+    @Bean
+    // @Primary
+    @Scope(scopeName = "prototype") // de tip prototype duuh => nu mai e singleton
+    public TransactionRepository transactionRepository2() { // daca vreau 2 bean-uri pe aceeasi clasa => eroare | 3 metode de a evita: qualifier, cu name: sau setam una ca fiind primara
+        TransactionRepository transactionRepository = new TransactionRepository();
+        transactionRepository.setNume("Transaction 2");
+        return transactionRepository;
+    }
+}
---
+++ soj2-StartingWithSpring/src/main/java/main/Main.java
@@ -0,0 +1,36 @@
+package main;
+
+import beans.TransactionRepository;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        try( AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ProjectConfig.class)) { // mergi in clasa respectiva si preia configurarile de acolo
+
+//            TransactionRepository transactionRepository = context.getBean(TransactionRepository.class); // by type
+            TransactionRepository transactionRepository = context.getBean("transactionRepository", TransactionRepository.class); //by name
+//            TransactionRepository transactionRepository2 = context.getBean("transactionRepository2", TransactionRepository.class); //by name
+            TransactionRepository transactionRepository2 = context.getBean("transactionRepository", TransactionRepository.class); //by name
+
+            transactionRepository2.setNume("Singleton"); // daca avem acelasi bean => ac instanta
+
+            System.out.println(transactionRepository);
+            System.out.println(transactionRepository2);
+
+
+            
+
+            TransactionRepository transactionRepositoryPrototype = context.getBean("transactionRepository2", TransactionRepository.class); //by name
+            TransactionRepository transactionRepositoryPrototype2 = context.getBean("transactionRepository2", TransactionRepository.class); //by name
+
+            transactionRepositoryPrototype2.setNume("Prototype"); // => instante diferite
+
+            System.out.println(transactionRepositoryPrototype);
+            System.out.println(transactionRepositoryPrototype2);
+        }
+
+        // context.close(); - cu try nu mai e nevoie sa inchidem manual contextul
+    }
+}
