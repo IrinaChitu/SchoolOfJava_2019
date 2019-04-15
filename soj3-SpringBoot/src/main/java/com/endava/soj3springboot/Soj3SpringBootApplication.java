e1bd49765d6239f94d59b69bb0c5d0ff8a49f833
Irina <chitu.irina.nicoleta@gmail.com>
2019-04-15 18:20:25 +0300

    SringBoot

---
+++ soj3-SpringBoot/src/main/java/com/endava/soj3springboot/Soj3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.endava.soj3springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soj3SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Soj3SpringBootApplication.class, args);
+    }
+
+}
